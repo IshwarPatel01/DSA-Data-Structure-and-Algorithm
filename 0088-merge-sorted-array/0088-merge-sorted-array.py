9bf138834cc9fd4c8c51ebe38d0808a31ed21839
Ishwar Patel <ishwarrpatel01@gmail.com>
2025-02-13 19:42:44 +0530

    Time: 0 ms (100%), Space: 17.7 MB (89.33%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -1,22 +1,15 @@
-class Solution(object):
-    def merge(self, nums1, m, nums2, n):
-        # Pointers for nums1, nums2, and the merged array
-        nums1_index = m - 1  # Last valid element in nums1
-        nums2_index = n - 1  # Last element in nums2
-        merge_index = m + n - 1  # Last position in nums1
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        midx = m - 1
+        nidx = n - 1 
+        right = m + n - 1
 
-        # Merge nums1 and nums2 starting from the end
-        while nums1_index >= 0 and nums2_index >= 0:
-            if nums1[nums1_index] > nums2[nums2_index]:
-                nums1[merge_index] = nums1[nums1_index]
-                nums1_index -= 1
+        while nidx >= 0:
+            if midx >= 0 and nums1[midx] > nums2[nidx]:
+                nums1[right] = nums1[midx]
+                midx -= 1
             else:
-                nums1[merge_index] = nums2[nums2_index]
-                nums2_index -= 1
-            merge_index -= 1
+                nums1[right] = nums2[nidx]
+                nidx -= 1
 
-        # If there are still elements in nums2, copy them
-        while nums2_index >= 0:
-            nums1[merge_index] = nums2[nums2_index]
-            nums2_index -= 1
-            merge_index -= 1
+            right -= 1
