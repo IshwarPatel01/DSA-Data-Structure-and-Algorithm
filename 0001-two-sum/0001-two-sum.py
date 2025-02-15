d73cc6d349a86c2ebc2b3e2d4f66a79d6d0d91e6
Ishwar Patel <ishwarrpatel01@gmail.com>
2025-02-15 20:08:53 +0530

    Time: 1765 ms (23.2%), Space: 18.6 MB (64.1%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,16 +1,18 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        # seen = {}
-        # for i, num in enumerate(nums):
-        #     needed = target - num
-        #     if needed in seen:
-        #         return [seen[needed], i]
-        #     seen[num] = i
-        # return none
-        for num in range(len(nums)):
-            for i in range(num + 1, len(nums)):
-                if nums[num] + nums[i] == target:
-                    return [num , i]
-
-       
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
         
+        
+        for i in range(0,(len(nums) - 1)):
+            for j in range(i + 1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i,j]
+        return []
+                    
+# function twoSum(nums, target):
+#     hash_map = {}  # To store numbers and their indices
+#     for i, num in enumerate(nums):
+#         complement = target - num
+#         if complement in hash_map:
+#             return [hash_map[complement], i]  # Return indices of the pair
+#         hash_map[num] = i  # Store the current number and its index
+#     return []  # No solution found (though the problem guarantees one)
