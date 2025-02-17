3ab9120a74a8ef37a9671ef38a38543ee40779bd
Ishwar Patel <ishwarrpatel01@gmail.com>
2025-02-17 15:36:25 +0530

    Time: 3 ms (98.59%), Space: 18.3 MB (80.3%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.py
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.py
@@ -1,30 +1,52 @@
+# class Solution:
+#     def maxProduct(self, nums: List[int]) -> int:
+#         if not nums:
+#             return 0
+    
+#         max_product = nums[0]  # Initialize the result with the first element
+    
+#     # Outer loop: starting index of the subarray
+#         for i in range(len(nums)):
+#             current_product = 1  # Reset current_product for each new starting index
+        
+#         # Inner loop: ending index of the subarray
+#             for j in range(i, len(nums)):
+#                 current_product *= nums[j]  # Multiply the current element
+            
+#             # Update max_product if the current_product is greater
+#                 if current_product > max_product:
+#                     max_product = current_product
+    
+#         return max_product
+
+
+
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
+        max_current = nums[0]
+        min_current = nums[0]
+        max_product = nums[0]
 
-        if not nums:
-            return 0
-    
-        min_current = max_current = max_product = nums[0]
-    
         for i in range(1, len(nums)):
-        # If the current number is negative, swap min and max
-            if nums[i] < 0:
-                min_current, max_current = max_current, min_current
-        
-        # Update min_current without using the built-in min function
-            if nums[i] < min_current * nums[i]:
-                min_current = nums[i]
+            current_value = nums[i]
+
+            if current_value < 0:  # check negative value
+                temp = max_current
+                max_current = min_current
+                min_current = temp
+            
+            if current_value > max_current * current_value:
+                max_current = current_value 
+            else: 
+                max_current *= current_value
+            
+            if current_value < min_current * current_value:
+                min_current = current_value
             else:
-                min_current = min_current * nums[i]
-        
-        # Update max_current without using the built-in max function
-            if nums[i] > max_current * nums[i]:
-                max_current = nums[i]
-            else:
-                max_current = max_current * nums[i]
-        
-            # Update the overall maximum product
-            if max_current > max_product:
+                min_current *= current_value
+
+            if max_product < max_current:
                 max_product = max_current
-    
+
         return max_product
+
