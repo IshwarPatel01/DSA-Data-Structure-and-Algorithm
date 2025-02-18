cf8523ce683ea86ad4ad49103ffc72e8c84038ef
Ishwar Patel <ishwarrpatel01@gmail.com>
2025-02-18 17:49:48 +0530

    Time: 48 ms (5.74%), Space: 26.7 MB (97.38%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -1,12 +1,15 @@
-class Solution(object):
-    def containsDuplicate(self, nums):
-        #Creating a data structure to store each element in array we scan through
-        hash_table = {}
-        #Going through each element in array
-        for p in range(len(nums)):
-            #If the current element is not in a data structure we created yet then append current element else we know that array is duplicated
-            if hash_table.get(nums[p]):
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        nums.sort()
+
+        for i in range(len(nums)-1):
+            if nums[i] == nums[i + 1]:
                 return True
-            else:
-                hash_table[nums[p]] = True
+            
+                
         return False
+
+                    
+        
+        
+        
