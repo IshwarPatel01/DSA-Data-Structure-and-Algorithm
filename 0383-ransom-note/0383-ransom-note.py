e43fe88abd11776c8ce9bf0ec6a38dd15b43cac8
Ishwar Patel <ishwarrpatel01@gmail.com>
2025-03-25 13:03:32 +0530

    Time: 28 ms (17.05%), Space: 18.1 MB (35.15%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -1,16 +1,29 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        freq1 = {} 
-        freq2 = {}
-        for char in ransomNote:
-            freq1[char] = freq1.get(char,0) + 1
+        # freq1 = {} 
+        # freq2 = {}
+        # for char in ransomNote:
+        #     freq1[char] = freq1.get(char,0) + 1
         
-        for char in magazine:
-            freq2[char] = freq2.get(char,0) + 1
+        # for char in magazine:
+        #     freq2[char] = freq2.get(char,0) + 1
 
-        for char in freq1:
-            if freq1[char] > freq2.get(char, 0):
+        # for char in freq1:
+        #     if freq1[char] > freq2.get(char, 0):
+        #         return False
+        # return True
+
+        
+        freq = {}
+
+        for char in magazine:
+            freq[char] = freq.get(char,0) + 1
+
+        for char in ransomNote:
+            if freq.get(char,0) == 0:
                 return False
+            freq[char] -= 1
+        
         return True
 
-        
+
