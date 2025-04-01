d3a65aecb3f0bc306b4559d039b8b0f2df8cb390
Ishwar Patel <ishwarrpatel01@gmail.com>
2025-04-01 17:42:48 +0530

    Time: 7 ms (38.91%), Space: 18.7 MB (73.04%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,8 +1,17 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        res = len(nums)
+        n = len(nums)
+        hash_map = [0] * (n + 1)
 
         for i in range(len(nums)):
-            res += i - nums[i]
+            hash_map[nums[i]] += 1
         
-        return res
+        for i in range(len(hash_map)):
+            if hash_map[i] == 0:
+                return i
+            
+        return len(hash_map)
+
+
+
+        
