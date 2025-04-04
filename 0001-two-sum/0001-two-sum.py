600cece558beea1452666dc9d9d445265d063e2a
Ishwar Patel <ishwarrpatel01@gmail.com>
2025-04-04 22:00:48 +0530

    Time: 0 ms (100%), Space: 18.8 MB (51.46%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,26 +1,26 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        # dist = {}
-        # for i in range(len(nums)):
-        #     complement = target - nums[i]
-        #     if complement in dist:
-        #         return [dist[complement], i]
+        dist = {}
+        for i in range(len(nums)):
+            complement = target - nums[i]
+            if complement in dist:
+                return [dist[complement], i]
             
-        #     dist[nums[i]] = i
-        # return []    
-        nums_with_index = [(num, i) for i, num in enumerate(nums)]
-        n = len(nums)
-        left = 0
-        right = n - 1
-        nums_with_index.sort()
+            dist[nums[i]] = i
+        return []    
+        # nums_with_index = [(num, i) for i, num in enumerate(nums)]
+        # n = len(nums)
+        # left = 0
+        # right = n - 1
+        # nums_with_index.sort()
 
-        while left < right:
-            summ = nums_with_index[left][0] + nums_with_index[right][0]
-            if target == summ:
-                return [nums_with_index[left][1] , nums_with_index[right][1]]
-            elif target < summ:
-                right -= 1
-            else:
-                left += 1
-        return []   
+        # while left < right:
+        #     summ = nums_with_index[left][0] + nums_with_index[right][0]
+        #     if target == summ:
+        #         return [nums_with_index[left][1] , nums_with_index[right][1]]
+        #     elif target < summ:
+        #         right -= 1
+        #     else:
+        #         left += 1
+        # return []   
 
