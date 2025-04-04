3c902cb83bc3dba54b504c9286f8c50b0bad6cca
Ishwar Patel <ishwarrpatel01@gmail.com>
2025-04-04 15:37:35 +0530

    Time: 0 ms (100%), Space: 18.6 MB (73.13%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -35,8 +35,10 @@ class Solution:
         n = len(nums)
 
         expected_sum = n * (n + 1) // 2 
-
-        actual_sum = sum(nums)
+        actual_sum = 0
+        # actual_sum = sum(nums)
+        for i in nums:
+            actual_sum += i
         return expected_sum - actual_sum
         
 
