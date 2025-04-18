5cdc768b20361d7185eca66db36ecc8ffb8358b3
Ishwar Patel <ishwarrpatel01@gmail.com>
2025-04-18 10:46:46 +0530

    Time: 0 ms (100%), Space: 18.9 MB (51.75%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,26 +1,13 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        dist = {}
-        for i in range(len(nums)):
+        hash_map = {}
+        n = len(nums)
+        for i in range(n):
             complement = target - nums[i]
-            if complement in dist:
-                return [dist[complement], i]
+            if complement in hash_map:
+                return [hash_map[complement], i]
+            hash_map[nums[i]] = i
             
-            dist[nums[i]] = i
-        return []    
-        # nums_with_index = [(num, i) for i, num in enumerate(nums)]
-        # n = len(nums)
-        # left = 0
-        # right = n - 1
-        # nums_with_index.sort()
 
-        # while left < right:
-        #     summ = nums_with_index[left][0] + nums_with_index[right][0]
-        #     if target == summ:
-        #         return [nums_with_index[left][1] , nums_with_index[right][1]]
-        #     elif target < summ:
-        #         right -= 1
-        #     else:
-        #         left += 1
-        # return []   
 
+        
