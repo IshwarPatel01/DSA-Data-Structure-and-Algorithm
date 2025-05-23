29fc53822606abeff1cb587e5341e75a0efbc33e
Ishwar Patel <ishwarrpatel01@gmail.com>
2025-05-23 12:33:09 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Nth Fibonacci Number/nth-fibonacci-number.py
+++ Difficulty: Easy/Nth Fibonacci Number/nth-fibonacci-number.py
@@ -6,25 +6,6 @@ class Solution:
             return 0
         if n == 1:
             return 1
-        if n <= 2:
+        if n == 2:
             return 1
-        return self.nthFibonacci(n - 1) + self.nthFibonacci(n -2)
-
-
-
-#{ 
- # Driver Code Starts
-if __name__ == "__main__":
-    t = int(input())
-    for _ in range(t):
-
-        n = int(input())
-
-        obj = Solution()
-        res = obj.nthFibonacci(n)
-
-        print(res)
-
-        print("~")
-
-# } Driver Code Ends
+        return self.nthFibonacci(n - 1) + self.nthFibonacci(n - 2)
