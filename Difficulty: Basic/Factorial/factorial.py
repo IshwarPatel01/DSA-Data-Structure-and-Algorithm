8da391d3ca68ea8651db304735467d08d9ab0588
Ishwar Patel <ishwarrpatel01@gmail.com>
2025-06-03 18:05:10 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Factorial/factorial.py
+++ Difficulty: Basic/Factorial/factorial.py
@@ -1,5 +1,9 @@
+#User function Template for python3
+
+
 class Solution:
-    def factorial (self, n):
+    def factorial(self, n):
         if n == 0:
             return 1
-        return n * self.factorial(n - 1)
+        return  n * self.factorial(n - 1)
+        # code here
