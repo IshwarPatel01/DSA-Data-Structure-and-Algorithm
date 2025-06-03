3c6b9a1d53ad36015ec3fc06ecf95dc9eed009cd
Ishwar Patel <ishwarrpatel01@gmail.com>
2025-06-03 18:07:41 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Sum of Natural Numbers/sum-of-natural-numbers.py
+++ Difficulty: Basic/Sum of Natural Numbers/sum-of-natural-numbers.py
@@ -1,7 +1,7 @@
 
 class Solution:
     def seriesSum(self, n : int) -> int:
-        if n == 0:
-            return 0
+        if n == 1:
+            return 1 
         return n + self.seriesSum(n - 1)
         
